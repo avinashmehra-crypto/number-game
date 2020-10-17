9668206955f7e7d575d23cd9dad2d3a671b0f434
avinashmehra-crypto <73026188+avinashmehra-crypto@users.noreply.github.com>
2020-10-17 22:16:25 +0530

    game.py

---
+++ game.py
@@ -0,0 +1,9 @@
+#python 2.7.15
+
+print ("PICK A NUMBER! ")
+print ("MULTIPLY WITH 2!")
+print ("ADD 10!")
+print ("DIVIDE BY 2!")
+num = int(input("Enter Your Result :"))
+print "The number is :", num - 5
+print ("If you want to play again, rerun the code")
